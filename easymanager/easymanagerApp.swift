008f89fa3dabc0aee5a8163518e5f051f5051a0d
Samuele Segrini <samuele.segrini@mail.polimi.it>
2023-04-01 11:32:52 +0200

    Initial Commit

---
+++ easymanager/easymanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  easymanagerApp.swift
+//  easymanager
+//
+//  Created by Samuele Segrini on 01/04/23.
+//
+
+import SwiftUI
+
+@main
+struct easymanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
